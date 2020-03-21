a497bb7f0912ff4edac653dc4e16f46fec01d9c7
RocketFirm <rocketfirm@Tamerlan.Dlink>
2020-03-21 23:46:05 +0600

    feat: add custom fonts

---
+++ src/components/Common/Typography.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import { View, Text } from 'react-native'
+import styled from 'styled-components';
+
+export const Title = styled.Text`
+    font-family: 'SFProDisplay-SemiBold';
+    font-size: 36px;
+`
--- src/views/Library/index.js
+++ src/views/Library/index.js
@@ -1,6 +1,10 @@
 import React, { useEffect } from 'react'
 import { SafeAreaView, Text } from 'react-native'
 
+/* DEV */
+import { Title } from 'components/Common/Typography';
+
+
 /* REDUX */
 import { connect } from 'react-redux';
 import { fetchCurrentSeason, fetchSeason } from 'store/season/season.action.js';
@@ -15,12 +19,13 @@ const Library = props => {
     // props.fetchTodaySchedule();
     // props.fetchSeason({year: 1999, season: 'summer'});
     // props.fetchAnime({id: 23})
-    props.fetchManga({id: 23, request: 'pictures'});
+    // props.fetchManga({id: 23, request: 'pictures'});
+    
   }, [])
 
   return (
     <SafeAreaView>
-      <Text>This is the library screen</Text>
+      <Title>FONTS</Title>
     </SafeAreaView>
   )
 }
